3e6192bd66ad8bbbb770794d62656b09d1c9a9ca
NikitaPilez <pileznikita99@gmail.com>
2023-01-27 14:20:17 +0300

    Deploy conf

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+tmp=/dev/shm/membot
+rm -rf ${tmp} && \
+git clone -l -s . ${tmp} -b master && \
+cd ${tmp} && \
+echo "Building..." && \
+composer i --no-dev --optimize-autoloader && \
+rm -rf .env.example .gitattributes composer.lock .git* README.md && \
+echo "Deploying..." && \
+rsync -av --delete --exclude ".env" --exclude "/storage" ./ user@142.93.102.201:membot/ && \
+ssh user@142.93.102.201 "cd membot && php artisan migrate && php artisan optimize && php artisan route:cache && php artisan cache:clear" && \
+echo "Done in ${SECONDS} sec."
+
