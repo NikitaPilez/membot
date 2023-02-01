73c9b1a9871fab1f262764a3e4e31f14a37752b3
CyberVladislav <foreversmiledface@mail.ru>
2023-02-01 16:53:33 +0300

    Send video to telegram command and job

---
+++ app/Console/Commands/SendVideoTelegramCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SendVideoTelegramJob;
+use App\Models\Video;
+use App\Services\GoogleDriveService;
+use Illuminate\Console\Command;
+
+class SendVideoTelegramCommand extends Command
+{
+    protected $signature = 'send:video-telegram';
+
+    protected $description = 'Отправляет видео в телеграм канал';
+
+    public function handle(GoogleDriveService $googleDriveService)
+    {
+        $files = $googleDriveService->getFiles(config('services.google.mem_video_folder_id'));
+
+        $googleDriveService->downloadFiles($files);
+
+        $video = Video::query()->where('is_sent', 0)->inRandomOrder()->first();
+
+        SendVideoTelegramJob::dispatch($video);
+    }
+}
