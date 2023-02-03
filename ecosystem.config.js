90c485659a45c77573c78ba21831e1d07693c3b7
NikitaPilez <pileznikita99@gmail.com>
2023-02-03 11:51:10 +0300

    Schedule run

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+        {
+            name: 'websocket',
+            interpreter: '/usr/bin/php8.1',
+            script: './artisan',
+            args: 'schedule:run',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '256M',
+            kill_timeout: 20000,
+            restart_delay: 5000,
+            max_restarts: 15,
+        },
+    ],
+};
