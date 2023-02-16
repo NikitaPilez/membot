6ee1dda43662fbb760328994ced78e6d2df11c13
NikitaPilez <pileznikita99@gmail.com>
2023-02-16 12:23:13 +0300

    Fix.

--- app/Helpers/Download/YoutubeDownloadVideo.php
+++ app/Helpers/Download/YoutubeDownloadVideo.php
@@ -12,20 +12,9 @@ class YoutubeDownloadVideo implements DownloadVideoInterface
 
     public function download(string $url)
     {
-        $browserFactory = new BrowserFactory();
-        $browser = $browserFactory->createBrowser();
+        preg_match('/"(?=https:\/\/rr).{1,444}(?=mp4).*?"/', file_get_contents($url), $matches);
+        $videoUrl = str_replace('\u0026', '&', str_replace('"', '', $matches[0]));
 
-        $page = $browser->createPage();
-        $page->navigate($url)->waitForNavigation(Page::NETWORK_IDLE);
-        $html = $page->getHtml(20000);
-
-        preg_match('/"(?=https:\/\/rr).{1,444}(?=mp4).*?"/', $html, $matches);
-
-        if (!$videoUrlWithBrackets = current($matches)) {
-            // TODO Error
-        }
-
-        $videoUrl = str_replace('\u0026', '&', str_replace('"', '', $videoUrlWithBrackets));
         Log::info("Downloaded youtube video, content url " . $videoUrl);
 
         app(GoogleDriveService::class)->createFile(file_get_contents($videoUrl));
