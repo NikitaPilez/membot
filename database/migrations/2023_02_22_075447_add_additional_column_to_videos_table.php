a2ce1c61c4f165d3d5e52b0e4b7eb31a4c9cf132
NikitaPilez <pileznikita99@gmail.com>
2023-02-22 14:11:24 +0300

    Migration

---
+++ database/migrations/2023_02_22_075447_add_additional_column_to_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->renameColumn("file_id", "google_file_id");
+            $table->string("url", 55)->nullable();
+            $table->text("content_url")->nullable();
+            $table->string("type", 55)->nullable();
+            $table->dateTime("sent_at")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            //
+        });
+    }
+};
