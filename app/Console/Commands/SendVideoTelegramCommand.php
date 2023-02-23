e386d98834e5bada8febd6fa0b5de58f9e76678c
NikitaPilez <pileznikita99@gmail.com>
2023-02-23 16:23:04 +0300

    Fix

--- app/Console/Commands/SendVideoTelegramCommand.php
+++ app/Console/Commands/SendVideoTelegramCommand.php
@@ -20,8 +20,14 @@ class SendVideoTelegramCommand extends Command
         $videoIds = Video::pluck('google_file_id')->toArray();
 
         $video = Video::where("is_sent", 0)->first();
+
         if ($video) {
-            SendVideoTelegramJob::dispatch($video);
+            foreach ($files as $file) {
+                if ($file->getId() == $video->google_file_id) {
+                    $video = $googleDriveService->downloadFile($file);
+                    SendVideoTelegramJob::dispatch($video);
+                }
+            }
         } else {
             foreach ($files as $file) {
                 if (!in_array($file->id, $videoIds)) {
