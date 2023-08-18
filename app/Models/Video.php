d2dba0d2172cebe5bb251c1a83d71816af6efcab
Nikita Pilets <pileznikita99@gmail.com>
2023-08-18 16:08:39 +0300

    Crud video.

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
+    use CrudTrait;
     use HasFactory;
 
     protected $fillable = [
