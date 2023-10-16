0a460ca01f0e5bfaacf21a13c1bc83ce294a3986
Nikita Pilets <pileznikita99@gmail.com>
2023-10-16 17:23:58 +0300

    Drop backpack, install filament

---
+++ app/Filament/Resources/VideoResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VideoResource\Pages;
+use App\Filament\Resources\VideoResource\RelationManagers;
+use App\Models\Video;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VideoResource extends Resource
+{
+    protected static ?string $model = Video::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('google_file_id'),
+                Tables\Columns\ToggleColumn::make('is_sent'),
+                Tables\Columns\TextColumn::make('url'),
+                Tables\Columns\TextColumn::make('content_url'),
+                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('sent_at')->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVideos::route('/'),
+            'create' => Pages\CreateVideo::route('/create'),
+            'edit' => Pages\EditVideo::route('/{record}/edit'),
+        ];
+    }
+}
