4d1450e5938244dc4780767fdebb95b6a76e3001
CyberVladislav <foreversmiledface@mail.ru>
2023-10-28 16:26:14 +0300

    ins

--- app/Helpers/Download/InstagramContentVideo.php
+++ app/Helpers/Download/InstagramContentVideo.php
@@ -4,11 +4,10 @@ namespace App\Helpers\Download;
 
 use App\DTO\GetContentUrlDTO;
 use Exception;
-use Illuminate\Support\Facades\Http;
+use HeadlessChromium\BrowserFactory;
 
 class InstagramContentVideo implements ContentVideoInterface
 {
-
     public function getContent(string $sourceUrl)
     {
         return file_get_contents($sourceUrl);
@@ -17,25 +16,28 @@ class InstagramContentVideo implements ContentVideoInterface
     public function getContentUrl(string $videoUrl): GetContentUrlDTO
     {
         try {
-            $response = Http::get('sssinstagram.com');
-            $cookies = $response->cookies();
-
-            foreach ($cookies as $cookie) {
-                if ($cookie->getName() == 'sssinstagram_session') {
-                    $headers['Cookie'] = 'sssinstagram_session=' . $cookie->getValue();
-                }
-
-                if ($cookie->getName() == 'XSRF-TOKEN') {
-                    $headers['X-Xsrf-Token'] = urldecode($cookie->getValue());
-                }
-            }
-
-            $result = Http::withHeaders($headers)->post('https://sssinstagram.com/r', [
-                'link' => $videoUrl,
-                'token' => '',
+            $browserFactory = new BrowserFactory();
+            $browser = $browserFactory->createBrowser([
+                'customFlags' => ['--disable-blink-features', '--disable-blink-features=AutomationControlled'],
+                'sendSyncDefaultTimeout' => 10000,
             ]);
 
-            $sourceUrl = $result['data']['items'][0]['urls'][0]['urlDownloadable'];
+            $page = $browser->createPage();
+            $page->navigate('https://sssinstagram.com')->waitForNavigation();
+            $elem = $page->dom()->querySelector('#main_page_text');
+
+            $elem->click();
+            $elem->sendKeys($videoUrl);
+
+            sleep(1);
+
+            $page->evaluate("document.querySelector('#submit').click()");
+
+            sleep(5);
+
+            $sourceUrl = $page->evaluate("document.querySelector('.download-wrapper a').href")->getReturnValue();
+
+            $browser->close();
         } catch (Exception $exception) {
             return new GetContentUrlDTO(
                 success: false,
