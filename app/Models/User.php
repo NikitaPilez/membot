3b37c45d3778e8ddca02035f36b51f0a68a7494d
Nikita Pilets <pileznikita99@gmail.com>
2023-10-30 10:36:01 +0300

    Fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return Auth::user()->is_admin;
+        return Auth::user()->is_admin == 1;
     }
 }
