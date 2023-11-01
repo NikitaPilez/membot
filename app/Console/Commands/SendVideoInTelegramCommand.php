ca8401a312fc73365c8d865d0e13d360a0950d6f
Nikita Pilets <pileznikita99@gmail.com>
2023-11-01 16:07:59 +0300

    Send video command

--- app/Console/Commands/SendVideoInTelegramCommand.php
+++ app/Console/Commands/SendVideoInTelegramCommand.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
+use App\Models\Video;
 use App\Services\SendVideoInTelegramService;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class SendVideoInTelegramCommand extends Command
 {
@@ -15,6 +17,19 @@ class SendVideoInTelegramCommand extends Command
 
     public function handle(SendVideoInTelegramService $service): void
     {
-        $service->sendVideoInTelegram();
+        $video = Video::where('google_file_id', '!=', null)
+            ->where('is_sent', 0)
+            ->where('publication_date', '>', now())
+            ->orderBy('publication_date')
+            ->first();
+
+        if ($video) {
+            Log::channel('content')->info('Найдено видео для отправки', [
+                'video' => $video->id,
+                'link' => $video->url,
+            ]);
+
+            $service->sendVideoInTelegram($video);
+        }
     }
 }
